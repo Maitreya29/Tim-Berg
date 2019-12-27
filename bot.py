5eb02f854774521ddab4966c3cfc237d44c39feb
maitreya <maitreyapatni30@gmail.com>
2019-12-27 17:56:30 +0530

    gay

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ async def about(ctx):
 
 @client.command(aliases=['8ball','eightball'])
 async def _8ball(ctx, *, question):
-    responses= [' It is certain','It is decidedly so','Without a doubt.','Yes - definitely.','You may rely on it.','As I see it, yes.','Most likely','Outlook good.','Yes.','Signs point to yes','try again','try again','try again','try again','try again','I dont think so, no.','My reply is no','I dont see that happening','I am sorry but, no.','Eh, no.']
+    responses= ['ewww no.','eww no.']
     await ctx.send(f'Question: {question} \nAnswer: {random.choice(responses)}')
 
 @client.command()

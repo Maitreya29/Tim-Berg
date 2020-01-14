ae4e77bfcd164afa316d988e2634f73ce6f967d2
Maitreya Panti <itsme@maitreyap.xyz>
2020-01-14 19:57:42 +0530

    leak

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ import youtube_dl
 
 
 client = commands.Bot(command_prefix='.')
-censored=['PAB']
+censored=['Indian Kebab','IndianKebab','Indian kebab','indiankebab']
 
 players = {}
  
@@ -19,18 +19,18 @@ async def on_ready():
     print('Hello, I am ready')
 @client.event
 async def on_member_join(member):
-    await channel.send(f'{member} Thank you for joining')
+    await member.send(f'{member} Thank you for joining')
 
 @client.event
 async def on_member_remove(member):
-    await member.send(f'{member} Thank you for joining.')
+    await member.send(f'{member} Sad to see you go.')
 
 
 @client.command()
 async def about(ctx):
     await ctx.send(f'I am a bot. I was developed by Maitreya Patni who is a 13 year old student. I can do multiple things that a bot must! incae you want any extra features to be added, please ping @MaitreyaPatni29#9239')
 
-@client.command(aliases=['8ball','eightball'])
+@client.command(aliases=['8Ball','eightball'])
 async def _8ball(ctx, *, question):
     responses= [' It is certain','It is decidedly so','Without a doubt.','Yes - definitely.','You may rely on it.','As I see it, yes.','Most likely','Outlook good.','Yes.','Signs point to yes','I dont think so, no.','My reply is no','I dont see that happening','I am sorry but, no.','Eh, no.']
     await ctx.send(f'Question: {question} \nAnswer: {random.choice(responses)}')
@@ -53,21 +53,15 @@ async def ban(ctx, member: discord.Member, *, reason=None):
     await member.ban(reason=reason)
     await ctx.send(f'@{member} was banned because {reason}')
 
-@client.command()
-async def helpme(ctx):
-    await ctx.send("Here is a list of commands currently Available \nyou can use .8ball (question) to use the 8 ball command.\nsample: .8ball am I gay?\nOutput: 8Ball replies with a random message\nyou can use .clear  to delete the last 6 messages.\nsample: .clear \noutput: last 6 messages are deleted \n(optional: you can add a value after .clear to delete a certain number of messages.\nsample: .clear 20 \noutput: This deletes last 20 messages\nyou can use .kick to kick a user.\nsample: .kick @xyz#6976 I don't like you \noutput: User is kicked, Bot prints '@xyz#6967 was kicked because I don't like you' \nyou can use .ban to ban a user. \nsample: .ban @xyz#6976 I don't like you \noutput: User is banned, Bot prints '@xyz#6967 was banned because I don't like you')")
-
 @client.command(aliases=['bmm','Bmm'])
 async def BMM(ctx):
     await ctx.author.send('BMM - https://dbree.org/v/39ec8b')
     await ctx.send('Check DM, link to Beat My Meat sent!')
 
-@client.command(aliases=['sneknudes'])
-async def nakedsnake(ctx):
-    await ctx.send('https://pbs.twimg.com/media/Ci1IszpWYAIfHi8?format=jpg&name=900x900 okay')
+@client.command(aliases=['leak','leakedsongs'])
+async def Leaks(ctx):
+    await ctx.author.send('https://mega.nz/#F!liIRUaSR!TjZVna3mHZWk6CRT3kiP2Q')
+    await ctx.send('Checks DM, Link to all leaked songs sent to DMs.')
 
-@client.command()
-async def beautifulpic(ctx):
-    await ctx.send('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcGI6UFeAaOV9bHLlQjzD-XIkL8mVqmKm9ThpRmAuvw8CP_qCi&s very beautiful image')
 client.run(os.environ['TOKEN'])
 
